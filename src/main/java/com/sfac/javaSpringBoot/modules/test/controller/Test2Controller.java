6f0398c70a3c1e19fff63f8810dea0b75b3f6b6b
csb-csb <1204053x>
2020-08-11 20:18:52 +0800

    配置

--- src/main/java/com/sfac/javaSpringBoot/modules/test/controller/Test2Controller.java
+++
@@ -1,4 +0,0 @@
-package com.sfac.javaSpringBoot.modules.test.controller;
-
-public class Test2Controller {
-}
--- src/main/java/com/sfac/javaSpringBoot/modules/test/controller/TestController.java
+++ src/main/java/com/sfac/javaSpringBoot/modules/test/controller/TestController.java
@@ -1,14 +1,108 @@
 package com.sfac.javaSpringBoot.modules.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
+
+@Controller
 @RequestMapping("/test")
 public class TestController {
-@GetMapping("/testDesc")
-@ResponseBody//代表接口返回类型
-public String testDesc(){
+    //Value读取配置文件的值
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+    @Value("${server.port}")
+    private int port;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.age}")
+    private int age;
+    @Value("${com.desc}")
+    private String desc;
+    @Value("${com.random}")
+    private String random;
+    @Autowired
+    private ApplicationTest applicationTest;
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getRandom() {
+        return random;
+    }
+
+    public void setRandom(String random) {
+        this.random = random;
+    }
+
+    @GetMapping("/logTest")
+    @ResponseBody
+    public String logTest() {
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "This is log test11111";
+    }
+
+    @GetMapping("/config")
+    @ResponseBody
+    public String configTest() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(port).append("----")
+                .append(name).append("----")
+                .append(age).append("----")
+                .append(desc).append("----")
+                .append(random).append("----").append("<br>");
+        sb.append(applicationTest.getPort()).append("----")
+                .append(applicationTest.getName()).append("----")
+                .append(applicationTest.getAge()).append("----")
+                .append(applicationTest.getDesc()).append("----")
+                .append(applicationTest.getRandom()).append("----").append("<br>");
+
+        return sb.toString();
+    }
+
+
+        @GetMapping("/testDesc")
+         @ResponseBody//代表接口返回类型
+         public String testDesc(){
+    //
     return "This is test module desc.";
 }
 
