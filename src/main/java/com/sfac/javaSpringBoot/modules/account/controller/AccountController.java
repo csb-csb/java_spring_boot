c0b55c0d360bdf89a4e73a3179148635ef4faf9b
csb-csb <1204053x>
2020-08-19 18:57:59 +0800

    8.19,针对前端头部，尾部，动态组件的引入，包括对应的框架全部引入

---
+++ src/main/java/com/sfac/javaSpringBoot/modules/account/controller/AccountController.java
@@ -0,0 +1,15 @@
+package com.sfac.javaSpringBoot.modules.account.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/users")
+    public String usersPage(){
+        return "index";
+    }
+}
